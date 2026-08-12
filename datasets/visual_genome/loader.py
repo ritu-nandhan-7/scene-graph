207bbe95259130a70ff8f3cfe46d9bc9d903090d
Ritu Nandhan <24f1001952@ds.study.iitm.ac.in>
2026-08-12 13:37:21 +0530

    bug fixes

--- datasets/visual_genome/loader.py
+++ datasets/visual_genome/loader.py
@@ -158,7 +158,15 @@ class VisualGenomeLoader:
 
         first_rel = int(self.h5["img_to_first_rel"][image_idx])
         last_rel = int(self.h5["img_to_last_rel"][image_idx])
+        print(
+            "DEBUG:",
+            image_idx,
+            "first_rel =", first_rel,
+            "last_rel =", last_rel
+        )
+
         if first_rel == -1 or last_rel == -1:
+            print("DEBUG: RETURNING EMPTY RELATIONSHIPS")
             return []
 
         relationships = []
